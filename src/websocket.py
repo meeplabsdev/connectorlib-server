e216394647185019027007bebd928f5e0be7354a
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-06-27 21:12:27 +0100

    prevent connection lost errors from logs (auto reconnect on client side will do the work)

--- src/websocket.py
+++ src/websocket.py
@@ -4,6 +4,7 @@ from typing import Any
 import websockets
 from colorama import Fore
 from websockets import server as wserver
+import websockets.exceptions
 from messages.base import BaseHandler
 from handlers import HANDLERS
 
@@ -21,55 +22,58 @@ class Websocket:
         self.de = definitions
 
     async def handler(self, websocket: wserver.WebSocketServerProtocol):
-        async for message in websocket:
-            data: dict[str, Any] = json.loads(message)
-            id = data.pop("id", "")
-            sess = data.pop("session", "")
+        try:
+            async for message in websocket:
+                data: dict[str, Any] = json.loads(message)
+                id = data.pop("id", "")
+                sess = data.pop("session", "")
 
-            try:
-                # TODO: check session
-                if False:
-                    sess = ""
+                try:
+                    # TODO: check session
+                    if False:
+                        sess = ""
 
-                extra = ""
-                if sess != "":
-                    extra += "(Authenticated)"
+                    extra = ""
+                    if sess != "":
+                        extra += "(Authenticated)"
 
-                print(f"{Fore.LIGHTBLUE_EX}|← {id}{Fore.LIGHTBLACK_EX} {extra}{Fore.RESET}")
-                for k in data.keys():
-                    if k != "authRequired":
-                        item: dict[str, Any] | list[dict[str, Any]] = data.get(k, {})
-                        if type(item) is list and len(item) > 1:
-                            r = f"[{item[0]}, ...]"
-                        else:
-                            r = item
-                        print(f"   {k}: {r}")
-
-                resp: dict[str, Any] = await HANDLERS.get(  # type: ignore
-                    id,
-                    BaseHandler,
-                )(
-                    self,
-                ).act(**data)
-
-                if resp is not None:  # type: ignore
-                    id = resp.pop("id", "")
-
-                    print(f"{Fore.LIGHTRED_EX}|→ {id}{Fore.RESET}")
-                    for k in resp.keys():
+                    print(f"{Fore.LIGHTBLUE_EX}|← {id}{Fore.LIGHTBLACK_EX} {extra}{Fore.RESET}")
+                    for k in data.keys():
                         if k != "authRequired":
-                            item: dict[str, Any] | list[dict[str, Any]] = resp.get(k, {})
+                            item: dict[str, Any] | list[dict[str, Any]] = data.get(k, {})
                             if type(item) is list and len(item) > 1:
                                 r = f"[{item[0]}, ...]"
                             else:
                                 r = item
                             print(f"   {k}: {r}")
 
-                    resp["id"] = id
-                    await websocket.send(json.dumps(resp))
+                    resp: dict[str, Any] = await HANDLERS.get(  # type: ignore
+                        id,
+                        BaseHandler,
+                    )(
+                        self,
+                    ).act(**data)
 
-            except Exception as e:
-                raise e
+                    if resp is not None:  # type: ignore
+                        id = resp.pop("id", "")
+
+                        print(f"{Fore.LIGHTRED_EX}|→ {id}{Fore.RESET}")
+                        for k in resp.keys():
+                            if k != "authRequired":
+                                item: dict[str, Any] | list[dict[str, Any]] = resp.get(k, {})
+                                if type(item) is list and len(item) > 1:
+                                    r = f"[{item[0]}, ...]"
+                                else:
+                                    r = item
+                                print(f"   {k}: {r}")
+
+                        resp["id"] = id
+                        await websocket.send(json.dumps(resp))
+
+                except Exception as e:
+                    raise e
+        except (OSError, websockets.exceptions.ConnectionClosedError, ConnectionResetError):
+            pass
 
     async def serve(self):
         self.server = await websockets.serve(self.handler, "localhost", 3000)  # type: ignore
