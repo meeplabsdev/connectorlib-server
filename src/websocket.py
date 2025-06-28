653fab4b921f8e2024b2ba468f899fb68b07fa1d
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-06-28 22:01:22 +0100

    feat: add BulkMessage support

--- src/websocket.py
+++ src/websocket.py
@@ -28,52 +28,66 @@ class Websocket:
                 id = data.pop("id", "")
                 sess = data.pop("session", "")
 
-                try:
-                    # TODO: check session
-                    if False:
-                        sess = ""
+                # TODO: check session
+                if False:
+                    sess = ""
 
-                    extra = ""
-                    if sess != "":
-                        extra += "(Authenticated)"
+                if sess == "" and data.get("authRequired"):
+                    pass
 
-                    print(f"{Fore.LIGHTBLUE_EX}|← {id}{Fore.LIGHTBLACK_EX} {extra}{Fore.RESET}")
-                    for k in data.keys():
-                        if k != "authRequired":
-                            item: dict[str, Any] | list[dict[str, Any]] = data.get(k, {})
-                            if type(item) is list and len(item) > 1:
-                                r = f"[{item[0]}, ...]"
-                            else:
-                                r = item
-                            print(f"   {k}: {r}")
+                if id == "BulkMessage":
+                    messages: list[dict[str, Any]] = data.get("messages", [])
+                    if len(messages) == 0:
+                        pass
 
-                    resp: dict[str, Any] = await HANDLERS.get(  # type: ignore
-                        id,
-                        BaseHandler,
-                    )(
-                        self,
-                    ).act(**data)
+                    for bdata in messages:
+                        await self.handle(websocket, bdata, bdata.pop("id", ""), sess)
+                else:
+                    await self.handle(websocket, data, id, sess)
 
-                    if resp is not None:  # type: ignore
-                        id = resp.pop("id", "")
-
-                        print(f"{Fore.LIGHTRED_EX}|→ {id}{Fore.RESET}")
-                        for k in resp.keys():
-                            if k != "authRequired":
-                                item: dict[str, Any] | list[dict[str, Any]] = resp.get(k, {})
-                                if type(item) is list and len(item) > 1:
-                                    r = f"[{item[0]}, ...]"
-                                else:
-                                    r = item
-                                print(f"   {k}: {r}")
-
-                        resp["id"] = id
-                        await websocket.send(json.dumps(resp))
-
-                except Exception as e:
-                    raise e
         except (OSError, websockets.exceptions.ConnectionClosedError, ConnectionResetError):
             pass
+
+    async def handle(self, websocket: wserver.WebSocketServerProtocol, data: dict[str, Any], id: str, sess: str):
+        try:
+            extra = ""
+            if sess != "":
+                extra += "(Authenticated)"
+
+            print(f"{Fore.LIGHTBLUE_EX}|← {id}{Fore.LIGHTBLACK_EX} {extra}{Fore.RESET}")
+            for k in data.keys():
+                if k != "authRequired":
+                    item: dict[str, Any] | list[dict[str, Any]] = data.get(k, {})
+                    if type(item) is list and len(item) > 1:
+                        r = f"[{item[0]}, ...]"
+                    else:
+                        r = item
+                    print(f"   {k}: {r}")
+
+            resp: dict[str, Any] = await HANDLERS.get(  # type: ignore
+                id,
+                BaseHandler,
+            )(
+                self,
+            ).act(**data)
+
+            if resp is not None:  # type: ignore
+                id = resp.pop("id", "")
+
+                print(f"{Fore.LIGHTRED_EX}|→ {id}{Fore.RESET}")
+                for k in resp.keys():
+                    if k != "authRequired":
+                        item: dict[str, Any] | list[dict[str, Any]] = resp.get(k, {})
+                        if type(item) is list and len(item) > 1:
+                            r = f"[{item[0]}, ...]"
+                        else:
+                            r = item
+                        print(f"   {k}: {r}")
+
+                resp["id"] = id
+                await websocket.send(json.dumps(resp))
+        except Exception as e:
+            raise e
 
     async def serve(self):
         self.server = await websockets.serve(self.handler, "localhost", 3000)  # type: ignore
