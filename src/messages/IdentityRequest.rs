6e6b9e343b16b069b6ea93ab12c68f7b046347e8
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-07-05 00:52:17 +0100

    feat: calculate expected value for IdentityRequest

--- src/messages/IdentityRequest.rs
+++ src/messages/IdentityRequest.rs
@@ -1,25 +1,45 @@
 use serde::{ Deserialize, Serialize };
+use libaes::Cipher;
 use uuid::Uuid;
 
-use crate::{ handlers::SocketMessage, session::Session };
+use crate::{ handlers::SocketResponse, session::Session };
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
 pub struct Message {
     uuid: String,
     username: String,
 }
 
-pub fn handle(msg: Message, _sess: &Session) -> Option<SocketMessage> {
-    println!("{} {}", msg.uuid, msg.username);
-    let id = Uuid::new_v4().to_string().replace("-", "");
-    println!("Generated nonce {}", id);
+#[derive(Serialize, Debug)]
+pub struct Response {
+    nonce: String,
+    iv: String,
+}
 
-    // return Some(
-    //     SocketMessage::IdentityRequest(Message {
-    //         uuid: "test".to_string(),
-    //         username: "test2".to_string(),
-    //     })
-    // );
+pub fn handle(msg: Message, sess: &Session) -> Option<SocketResponse> {
+    if msg.uuid.len() != 32 {
+        return None;
+    }
 
-    return None;
+    let _det = Uuid::new_v4().to_string().replace("-", "");
+    let _det = _det.chars();
+
+    let nonce = _det.clone().skip(16).take(16).collect::<String>();
+    let iv = _det.clone().take(16).collect::<String>();
+
+    let plaintext = format!("{}{}{}", nonce, msg.uuid, msg.username);
+    let plaintext = plaintext.as_bytes();
+
+    let key = b"This is the key!"; // key is 16 bytes, i.e. 128-bit
+    let cipher = Cipher::new_128(key);
+
+    let expected = cipher.cbc_encrypt(iv.as_bytes(), plaintext);
+    println!("Expecting {}", base16ct::lower::encode_string(&expected));
+
+    return Some(
+        SocketResponse::IdentityRequest(Response {
+            nonce: nonce,
+            iv: iv,
+        })
+    );
 }
