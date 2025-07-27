39c82520202ee42b384951e539a008382ae50818
meeplabsdev <meeplabsdev@noreply.users.github.com>
2025-07-27 01:29:00 +0100

    add new messages

--- src/handlers.rs
+++ src/handlers.rs
@@ -9,6 +9,9 @@ use crate::session::Session;
 pub enum SocketMessage {
     IdentityChallenge(IdentityChallenge::Message),
     IdentityRequest(IdentityRequest::Message),
+    NetworkData(NetworkData::Message),
+    PlayerJoin(PlayerJoin::Message),
+    PlayerQuit(PlayerQuit::Message),
 }
 
 #[allow(dead_code)]
@@ -17,11 +20,17 @@ pub enum SocketMessage {
 pub enum SocketResponse {
     IdentityChallenge(IdentityChallenge::Response),
     IdentityRequest(IdentityRequest::Response),
+    NetworkData(NetworkData::Response),
+    PlayerJoin(PlayerJoin::Response),
+    PlayerQuit(PlayerQuit::Response),
 }
 
 pub async fn handle(message: SocketMessage, sess: &mut Session) -> Option<SocketResponse> {
     match message {
         SocketMessage::IdentityChallenge(msg) => IdentityChallenge::handle(msg, sess).await,
         SocketMessage::IdentityRequest(msg) => IdentityRequest::handle(msg, sess).await,
+        SocketMessage::NetworkData(msg) => NetworkData::handle(msg, sess).await,
+        SocketMessage::PlayerJoin(msg) => PlayerJoin::handle(msg, sess).await,
+        SocketMessage::PlayerQuit(msg) => PlayerQuit::handle(msg, sess).await,
     }
 }
--- src/messages/mod.rs
+++ src/messages/mod.rs
@@ -1,3 +1,6 @@
 #![allow(non_snake_case)]
 pub mod IdentityChallenge;
 pub mod IdentityRequest;
+pub mod NetworkData;
+pub mod PlayerJoin;
+pub mod PlayerQuit;
